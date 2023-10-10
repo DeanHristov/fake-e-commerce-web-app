48262d75f3e461363506f57a7f14a4a0288fff80
D. Hristov <dean.hristovv@gmail.com>
2023-10-10 12:04:06 +0300

    Updated the useFetch hook and added tests for it

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -3,7 +3,7 @@
 import { useEffect, useState } from 'react';
 
 export interface IFetchResponse<T, E> {
-  data?: T;
+  data?: T | null;
   loading?: boolean;
   error?: E | Record<string, any> | null;
 }
@@ -12,34 +12,38 @@ const useFetch = <T, E>(
   url: string,
   options?: RequestInit,
 ): IFetchResponse<T, E> => {
-  const [data, setData] = useState<T>();
-  const [loading, setLoading] = useState<boolean>(false);
-  const [error, setError] = useState<E | Record<string, any> | null>(null);
+  const [response, setResponse] = useState<IFetchResponse<T, E>>({
+    data: null,
+    loading: false,
+    error: null,
+  });
 
   useEffect(() => {
     if (!url) return;
 
-    setLoading(true);
-    setError(null);
+    setResponse((prevState) => ({ ...prevState, loading: true }));
 
     (async () => {
-      const response = await fetch(
-        `${process.env.NEXT_PUBLIC_API_URL}${url}`,
-        options,
-      );
+      try {
+        const response = await fetch(
+          `${process.env.NEXT_PUBLIC_API_URL}${url}`,
+          options,
+        );
 
-      if (!response.ok) {
-        setLoading(false);
-        setError({ msg: response.statusText });
+        const data = await response.json();
+
+        setResponse((prevState) => ({ ...prevState, loading: false, data }));
+      } catch (error: any) {
+        setResponse({
+          error: error.statusText,
+          loading: false,
+          data: null,
+        });
       }
-
-      const data = await response.json();
-      setLoading(false);
-      setData(data);
     })();
   }, [url, options]);
 
-  return { data, loading, error };
+  return response;
 };
 
 export default useFetch;
