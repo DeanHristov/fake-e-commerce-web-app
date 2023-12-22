9fb15a9ef91598527f93a49bbcec72391569a790
D. Hristov <dean.hristovv@gmail.com>
2023-12-22 15:15:34 +0200

    forgotten change from previous commit

--- src/components/containers/HomePageView/HomePageView.tsx
+++ src/components/containers/HomePageView/HomePageView.tsx
@@ -25,7 +25,7 @@ const HomePageView: FC<IHomePageViewProps> = ({
     if (Utils.isNotEmpty(products)) {
       dispatch(initProducts(products));
     }
-  }, [dispatch, products, error]);
+  }, [dispatch, products]);
 
   return (
     <section className="mx-auto px-2 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 pb-16 relative">
